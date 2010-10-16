47bc0dd9dc3b6aa93eb11f943d7267412d43768a
Lucas Mazza <luc4smazza@gmail.com>
2010-10-15 21:32:21 -0300

    Oauth helper

---
+++ spec/acceptance/support/oauth.rb
@@ -0,0 +1,28 @@
+ACCESS_TOKEN = {
+    :access_token => "railstemplates"
+  }
+
+  GITHUB_INFO = {
+    :user => {
+      :name  => 'Tyler Durden',
+      :email => 'tyler@soapfactory.com'
+    }
+  }
+
+module OauthHelpers
+  def self.included(base)
+    base.class_eval do
+
+      def stub_oauth!
+        Devise::Oauth.short_circuit_authorizers!
+
+        Devise::Oauth.stub!(:facebook) do |client|
+          client.post('/login/oauth/access_token') { [200, {}, ACCESS_TOKEN.to_json] }
+          client.post('/api/v2/json/user/show') { [200, {}, GITHUB_INFO.to_json] }
+        end
+      end
+    end
+  end
+end
+
+RSpec.configuration.include OauthHelpers, :type => :acceptance

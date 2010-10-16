ca83ba5426674051d8700991bd971ed418eb39aa
Lucas Mazza <luc4smazza@gmail.com>
2010-10-15 21:29:11 -0300

    Basic spec setup

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -6,3 +6,23 @@ RSpec.configuration.include Capybara, :type => :acceptance
 
 # Put your acceptance spec helpers inside /spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+DatabaseCleaner.strategy = :truncation
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:each, :type => :acceptance) do
+   Capybara.reset_sessions! 
+    DatabaseCleaner.clean
+  end
+
+  # config.before(:each) do
+  #   Capybara.current_driver = :selenium if example.metadata[:js]
+  # end
+  # 
+  # config.after(:each) do
+  #   Capybara.use_default_driver if example.metadata[:js]
+  # end  
+
+end

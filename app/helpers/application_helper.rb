4a99b6be6f0c8eb809ea792d648d9cfdb422b553
Lucas Mazza <luc4smazza@gmail.com>
2010-10-16 06:47:03 -0300

    omg front-end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+require 'digest/md5'
+
 module ApplicationHelper
+
+  def gravatar_for(githubber)
+    hash = Digest::MD5.hexdigest(githubber.email)
+    image_tag "http://www.gravatar.com/avatar/#{hash}?s=40"
+  end
+
 end

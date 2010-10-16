6177bf67c499fdea7aef7da535eebd8ebe795d68
Lucas Mazza <luc4smazza@gmail.com>
2010-10-16 03:25:14 -0300

    fixing factory

--- spec/support/factories/templates.rb
+++ spec/support/factories/templates.rb
@@ -1,6 +1,7 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :template do |f|
+  f.title "Omg title!"
   f.gist_file "MyString"
   f.githubber { |x| x.association :githubber }
 end

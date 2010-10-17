6dc5a0247527b210a791ebeef4d94634ed2d8ceb
Lucas Mazza <luc4smazza@gmail.com>
2010-10-17 13:04:10 -0200

    search template

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,5 @@
 class SearchesController < ApplicationController
   def search
-    # TODO: Total Count + Pagination
     search = ThinkingSphinx.search(params[:search])
     @total = search.count
     @templates = search.paginate(:page => params[:page], :per_page => 10)

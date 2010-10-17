beb26c4888aa5ea60f926f2a6a3adbee688999f3
Lucas Mazza <luc4smazza@gmail.com>
2010-10-17 17:21:40 -0200

    Revert "testing the 404 behaviour"
    This reverts commit 06502865390ab42136a496c7eb357220fb1bb627.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     dashboard_path
   end
-
-  def rescue_action_in_public(exception)
-    case exception
-      when ActiveRecord::RecordNotFound, ActionController::UnknownController, 
-        ActionController::UnknownAction, ActionController::RoutingError
-        render :template => "pages/404", :status => 404
-      else
-        super
-    end
-  end
-  
-  def local_request?
-    false
-  end
   
   private
   def load_github_from_session

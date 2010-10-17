a04cedab340197bc7e9941f88570a9b3aeca5b35
Lucas Mazza <luc4smazza@gmail.com>
2010-10-17 02:57:33 -0200

    fixed like spec

--- spec/acceptance/liking_a_template_spec.rb
+++ spec/acceptance/liking_a_template_spec.rb
@@ -9,18 +9,19 @@ feature "Liking a template", %q{
   background do
     stub_oauth!
     sign_in!
+    visit homepage
   end
 
-  scenario "Liking a template" do
-    @user = Factory(:githubber)
-    @template = Factory(:template, :title => 'belem do para', :description => "Founded in 1616 by the Portuguese, Belém was the first European colony  on the Amazon but did not become part of Brazil until 1775. Its metropolitan area has approximately 2.09 million inhabitants. It is also known as Metropolis of the Brazilian Amazon region or Cidade das Mangueiras (city of mango trees) due to the number of those trees found in the city. The newer part of the city has modern buildings and skyscrapers.", :githubber => @user)
-    visit "/templates/#{@template.id}"
-    @template.likes.count.should == 0
-    pending "Fix this whole spec" do
-      expect {
-        click_link 'Like'
-      }.to change(current_githubber.likes(true), :count).by(1)
-    end
-    @template.likes(true).count.should == 1
+  scenario "Liking a template", :js => true do
+    template = Factory(:template, :title => 'belem do para', :description => "Trolololo.", :githubber => Factory(:githubber))
+    click_link "All templates"
+    click_link template.title
+    
+    page.should have_css "strong", :text => '0'
+    click_link 'Like it'
+
+    click_link "All templates"
+    click_link template.title
+    page.should have_css "strong", :text => '1'
   end
 end

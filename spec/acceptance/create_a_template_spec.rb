ce111dfa5fc74ca10631e1216d89b4e942bb2809
Lucas Mazza <luc4smazza@gmail.com>
2011-01-09 17:20:49 -0200

    cleaning some warnings

--- spec/acceptance/create_a_template_spec.rb
+++ spec/acceptance/create_a_template_spec.rb
@@ -5,7 +5,7 @@ feature "Creating A Template", %q{
   As a githubber
   I want to select a gist and a file from my github profile
 } do
-  
+
   background do
     stub_oauth!
     sign_in!
@@ -19,69 +19,66 @@ feature "Creating A Template", %q{
       response.stub(:body).and_return("REPO TEMPLATE")
     end
   end
-  
+
   scenario "trying to create a template without a valid url" do
     fill_in "github_url", :with => "http://google.com/"
-    click "Next"
+    click_link_or_button "Next"
 
     page.should have_css ".invalid", :text => "Looks like 'http://google.com/' is neither a gist or a file under a github repository."
   end
 
   scenario "successfully creating a template from a gist" do
     fill_in "github_url", :with => "http://gist.github.com/1337"
-    click "Next"
-    
+    click_link_or_button "Next"
+
     fill_in 'Title', :with => "My awesome template"
     fill_in 'Description', :with => "Blablablablablablablablabla"
     fill_in 'Tag list', :with => "rails, mysql"
     select "main.rb", :from => "Gist file"
-    click "Create!"
+    click_link_or_button  "Create!"
     page.should have_css "h2", :text => "My awesome template"
     page.should have_content("rails, mysql")
   end
-  
+
   scenario "checking for required title" do
     fill_in "github_url", :with => "http://gist.github.com/1337"
-    click "Next"
-    
+    click_link_or_button "Next"
+
     fill_in 'Title', :with => ""
     fill_in 'Description', :with => "Blablablablablablablablabla"
     fill_in 'Tag list', :with => "rails, mysql"
     select "main.rb", :from => "Gist file"
-    click "Create!"
+    click_link_or_button "Create!"
     within :css, "#template_title_input" do
       page.should have_content "can't be blank"
     end
   end
-  
+
   scenario "successfully creating a template from a file inside a repo" do
     fill_in "github_url", :with => "http://github.com/sabbre/base-template/blob/master/template.rb"
-    click "Next"
-    
+    click_link_or_button "Next"
+
     fill_in 'Title', :with => "My awesome template"
     fill_in 'Description', :with => "Blablablablablablablablabla"
     fill_in 'Tag list', :with => "rails, mysql"
-  
-    
-    click "Create!"
-  
+
+    click_link_or_button "Create!"
+
     page.should have_css "h2", :text => "My awesome template"
     page.should have_content("rails, mysql")
   end
-  
+
   scenario "checking for required title" do
     fill_in "github_url", :with => "http://github.com/sabbre/base-template/blob/master/template.rb"
-    click "Next"
-    
+    click_link_or_button "Next"
+
     fill_in 'Title', :with => ""
     fill_in 'Description', :with => "Blablablablablablablablabla"
     fill_in 'Tag list', :with => "rails, mysql"
-    click "Create!"
-  
+    click_link_or_button "Create!"
+
     within :css, "#template_title_input" do
       page.should have_content "can't be blank"
     end
   end
-  
-  
 end
